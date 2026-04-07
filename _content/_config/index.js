0c635f71dfe0199beda3167847b2a2219fbb1d24
Meriton <meritonaskaj01@gmail.com>
2026-04-07 14:49:39 +0200

    test add pages collection

--- _content/_config/index.js
+++ _content/_config/index.js
@@ -1,16 +1,42 @@
-// import {
-//   NODE_ENV,
-//   WORKING_DIR_ABSOLUTE,
-//   CONTENT_PATH_PREFIX,
-//   CONTENT_DIR,
-//   PROD_URL,
-//   DISPLAY_URL,
-//   CMS_AUTH_URL,
-//   CMS_REPO,
-//   CMS_BACKEND,
-//   CMS_BRANCH,
-// } from "../../env.config.js";
+import {
+  NODE_ENV,
+  SRC_DIR_FROM_WORKING_DIR,
+  WORKING_DIR_ABSOLUTE,
+  CONTENT_PATH_PREFIX,
+  CONTENT_DIR,
+  PROD_URL,
+  DISPLAY_URL,
+  CMS_AUTH_URL,
+  CMS_REPO,
+  CMS_BACKEND,
+  CMS_BRANCH,
+} from "../../env.config.js";
 
-export const collections = [];
+const { pages, spreadPageSetup } = await import(
+  `../${SRC_DIR_FROM_WORKING_DIR}/config-11ty/plugins/cms-config/index.js`
+);
+
+export const collections = [
+  {
+    ...pages,
+    ...spreadPageSetup("custom-pages"),
+    // Icon names can be found here: https://material.io/resources/icons/?style=baseline
+    icon: "exercise",
+
+        ...pages,
+    ...spreadPageSetup("fs"),
+    icon: "help",
+  },
+  {
+    ...pages,
+    ...spreadPageSetup("revreviews"),
+    icon: "reviews",
+  },
+  {
+    ...pages,
+    ...spreadPageSetup("plagces"),
+    icon: "place",
+  },
+];
 
 export const singletons = [];
